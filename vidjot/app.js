8644bfd74256e8ec1013aa1f154ccc5aa69cff29
Brian <baccarellab@gmail.com>
2019-07-13 17:05:14 -0400

    update app.js

--- vidjot/app.js
+++ vidjot/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const exphbs = require ('express-handlebars');
+const exphbs  = require('express-handlebars');
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
@@ -10,18 +10,20 @@ const app = express();
 
 // Load routes
 const ideas = require('./routes/ideas');
+const users = require('./routes/users');
 
-//Connect to mongoose
-mongoose.connect('mongodb://localhost/vidjot-brian', {
-  useNewUrlParser: true 
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+  useMongoClient: true
 })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-//Handlebars Middleware
+// Handlebars Middleware
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
+app.set('view engine', 'handlebars');
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,53 +32,43 @@ app.use(bodyParser.json());
 // Method override middleware
 app.use(methodOverride('_method'));
 
-app.set('view engine', 'handlebars');
-
-// Express session middleware
-app.use(session({ 
-  secret: 'secret', 
+// Express session midleware
+app.use(session({
+  secret: 'secret',
   resave: true,
-  saveUninitialized: true,
+  saveUninitialized: true
 }));
 
 app.use(flash());
 
 // Global variables
-app.use(function(request, response, next){
-  response.locals.success_msg = request.flash('success_msg');
-  response.locals.error_msg = request.flash('error_msg');
-  response.locals.error = request.flash('error');
+app.use(function(req, res, next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
   next();
 });
 
-//Index Route
-app.get('/', (request, response) => {
-  const title = 'Welcome to my first node.js app'
-  response.render('index', {
+// Index Route
+app.get('/', (req, res) => {
+  const title = 'Welcome';
+  res.render('index', {
     title: title
   });
 });
 
-//About Route
-app.get('/about', (request, response) => {
-  response.render('about');
+// About Route
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
-// User Login Route
-app.get('/users/login', (request, response) => {
-  response.send('login');
-})
-
-// User Register Route
-app.get('/users/register', (request, response) => {
-  response.send('register');
-})
 
 // Use routes
 app.use('/ideas', ideas);
+app.use('/users', users);
 
 const port = 5000;
 
-app.listen(port, () => {
+app.listen(port, () =>{
   console.log(`Server started on port ${port}`);
 });
