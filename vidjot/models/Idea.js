6a1c8df6fe7e1016a5023dafad05d7969d9d2bc6
Brian <baccarellab@gmail.com>
2019-07-15 12:10:07 -0400

    create unique user finish auth
    user's will only see their videos now.

--- vidjot/models/Idea.js
+++ vidjot/models/Idea.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const IdeaSchema = new Schema({
-  title:{
+  title: {
     type: String,
     required: true
   },
-  details:{
+  details: {
+    type: String,
+    required: true
+  },
+  user: {
     type: String,
     required: true
   },
